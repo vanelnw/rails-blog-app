855195e5fe0b2693a284dff8689fe6af49409905
Vanel <vanelnwaba@gmail.com>
2023-03-10 09:02:56 +0100

    update schema

--- spec/view/user_index_spec.rb
+++ spec/view/user_index_spec.rb
@@ -1,46 +1,42 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'User index page', type: :feature do
-  before do
-    user1 = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a teacher',
-                        posts_counter: 1)
-    user2 = User.create(name: 'Johni', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a chief',
-                        posts_counter: 2)
-    user3 = User.create(name: 'Jane', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a speaker',
-                        posts_counter: 1)
+RSpec.describe "User index page", type: :feature do
 
-    @users = [user1, user2, user3]
+    before do
+        user1 = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a teacher', posts_counter: 1)
+        user2 = User.create(name: 'Johni', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a chief', posts_counter: 2)
+        user3 = User.create(name: 'Jane', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'I am a speaker', posts_counter: 1)
+       
+        @users = [user1, user2, user3]
 
-    # allow(User).to receive(:all).and_return(@users)
-
-    visit users_path
-  end
-
-  it 'displays the username of all other users' do
-    @users.each do |user|
-      next if user == @users.first
-
-      expect(page).to have_content(user.name)
+       visit users_path
     end
-  end
 
-  it 'displays the profile picture for each user' do
-    @users.each do |user|
-      expect(page).to have_css("img[src*='#{user.photo}']")
+    it "displays the username of all other users" do
+        @users.each do |user|
+            next if user == @users.first
+            expect(page).to have_content(user.name)
+        end
     end
-  end
 
-  it 'displays the number of posts each user has written' do
-    @users.each do |user|
-      expect(page).to have_content("Number of posts: #{user.posts_counter}")
+    it "displays the profile picture for each user" do
+        @users.each do |user|
+          expect(page).to have_css("img[src*='#{user.photo}']")
+        end
     end
-  end
 
-  it 'redirects to the user show page when a user is clicked' do
-    user = @users[2]
+    it "displays the number of posts each user has written" do
+        @users.each do |user|
+          expect(page).to have_content("Number of posts: #{user.posts_counter}")
+        end
+    end
 
-    click_link(user.name)
+    it "redirects to the user show page when a user is clicked" do
+        
+        user = @users[2]
 
-    expect(page).to have_current_path(user_path(user.id))
-  end
+        click_link(user.name)
+
+        expect(page).to have_current_path(user_path(user.id))
+    end
 end
